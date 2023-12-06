87e95456cf7f7de4ff5210e5a6f68ac98d81731c
nahirniy <nahirniyy@gmail.com>
2023-12-06 21:51:40 +0200

    added request function (without post)

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import './home-content/home-content';
 import './filters/filters';
 import './modals/modal-product/modal-product';
 import './modals/modal-subscription/modal-subscription';
+import './services/food-api';
--- src/js/services/food-api.js
+++ src/js/services/food-api.js
@@ -1,1 +1,47 @@
 // ТУТ ПИШЕ ТІЛЬКИ МАКС
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://food-boutique.b.goit.study/api';
+
+export const getCategoryList = async () => {
+  const { data } = await axios.get(`/products/categories`);
+
+  return data;
+};
+
+export const getCurrentProducts = async ({ value, category, page, limit }) => {
+  const params = new URLSearchParams({
+    page,
+    limit,
+  });
+
+  if (value) {
+    params.set('value', value);
+  }
+
+  if (category) {
+    params.set('category', category);
+  }
+
+  const { data } = await axios.get(`/products/`, { params });
+
+  return data;
+};
+
+export const getProductById = async id => {
+  const { data } = await axios.get(`/products/${id}`);
+
+  return data;
+};
+
+export const getPopularProducts = async limit => {
+  const { data } = await axios.get(`/products/popular?limit=${limit}`);
+
+  return data;
+};
+
+export const getDiscountProducts = async () => {
+  const { data } = await axios.get(`/products/discount`);
+
+  return data;
+};
