9d97fa542123555aeddeacdf03956fd1a2d559f1
nahirniy <115656838+nahirniy@users.noreply.github.com>
2023-12-07 20:02:37 +0000

    Deploying to gh-pages from @ nahirniy/Code-Storm@723c71206c0e2ed29385ba5dca448fab8a7406a2 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-32f918d4.js";import{a as o}from"./assets/vendor-a61d8330.js";o.defaults.baseURL="https://food-boutique.b.goit.study/api";
+import"./assets/styles-32f918d4.js";import{a as o}from"./assets/vendor-4d3d87e9.js";o.defaults.baseURL="https://food-boutique.b.goit.study/api";
 //# sourceMappingURL=commonHelpers2.js.map
