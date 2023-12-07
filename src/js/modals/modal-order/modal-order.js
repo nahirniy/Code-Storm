d9c8f509eed817397d78551383cbe629beee421a
Vladyslav6 <vlad51284@gmail.com>
2023-12-08 00:12:20 +0200

    ModalOrder

--- src/js/modals/modal-order/modal-order.js
+++ src/js/modals/modal-order/modal-order.js
@@ -1,1 +1,11 @@
+const openBottun = document.querySelector(".modal_button")
+const clouseBottun = document.querySelector(".clouse_modal")
+const modalBackdrop = document.querySelector('.modal-backdrop-order')
+//////
+openBottun.addEventListener('click', toglModul)
+clouseBottun.addEventListener('click', toglModul)
+function toglModul(){
+    modalBackdrop.classList.toggle('is-hidden')
+}
+/////
 
