190c24db442fe94aaacdc08c86b2b2f65bf7c5c4
Inna <136462686+InnaKolomiets@users.noreply.github.com>
2023-12-08 22:25:21 +0200

    fix style

--- src/js/cart-content/order-products/order-products.js
+++ src/js/cart-content/order-products/order-products.js
@@ -1,24 +1,23 @@
 import { modalOrder } from "../../modals/modal-order/modal-order";
 import { addOrder } from "../../services/food-api";
 import { Notify } from "notiflix";
-document.addEventListener('DOMContentLoaded', () => {
-  const orderForm = document.querySelector('.order-products-form');
 
-    orderForm.addEventListener('submit', (event) => {
-      event.preventDefault();
+const orderForm = document.querySelector('.order-products-form');
+orderForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailInput = event.target.querySelector('#email');
+  const email = emailInput.value;
 
-      const emailInput = event.target.querySelector('#email');
-      const email = emailInput.value;
+  console.log('Order Details:');
+  console.log('Email:', email);
 
-      console.log('Order Details:');
-      console.log('Email:', email);
-      addOrder({ email: email, products: [] })
-        .then(() => {
-          modalOrder();
-        })
-        .catch(error => {
-          Notify.failure(error.message);
-        })
-      orderForm.reset();
-    });
-  });
+  addOrder({ email: email, products: [] })
+    .then(() => {
+      modalOrder();
+    })
+    .catch(error => {
+
+    })
+
+  orderForm.reset();
+});
