37f351785a084b8c11b3b9666af49fa30d602b33
Inna <136462686+InnaKolomiets@users.noreply.github.com>
2023-12-08 16:57:41 +0200

    order-products

--- src/js/cart-content/order-products/order-products.js
+++ src/js/cart-content/order-products/order-products.js
@@ -1,5 +1,9 @@
+import { modalOrder } from "../../modals/modal-order/modal-order";
+import { addOrder } from "../../services/food-api";
+import { Notify } from "notiflix";
 document.addEventListener('DOMContentLoaded', () => {
   const orderForm = document.querySelector('.order-products-form');
+
     orderForm.addEventListener('submit', (event) => {
       event.preventDefault();
 
@@ -8,7 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
       console.log('Order Details:');
       console.log('Email:', email);
-
+      addOrder({ email: email, products: [] })
+        .then(() => {
+          modalOrder();
+        })
+        .catch(error => {
+          Notify.failure(error.message);
+        })
       orderForm.reset();
     });
   });
--- src/js/modals/modal-order/modal-order.js
+++ src/js/modals/modal-order/modal-order.js
@@ -1,1 +1,17 @@
 
+export function modalOrder() {
+  const modalBackdropOrder = document.querySelector(".modal-backdrop-order");
+  const modalOrder = document.querySelector(".modal-order");
+
+  modalBackdropOrder.classList.remove("is-hidden");
+  modalOrder.classList.remove("is-hidden");
+
+}
+const modalOrderClose = document.querySelector(".modal-order-close");
+modalOrderClose.addEventListener("click", (event) => {
+  const modalBackdropOrder = document.querySelector(".modal-backdrop-order");
+  const modalOrder = document.querySelector(".modal-order");
+  modalBackdropOrder.classList.add("is-hidden");
+  modalOrder.classList.add("is-hidden");
+} );
+
