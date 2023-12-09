a3eb894ce047258ff150b4b962fe215c66057f0a
Vladyslav6 <vlad51284@gmail.com>
2023-12-09 05:22:48 +0200

    fixModal

--- src/js/modals/modal-product/modal-product.js
+++ src/js/modals/modal-product/modal-product.js
@@ -1,10 +1,72 @@
-const openBottun = document.querySelector(".modal_product_open")
+import { loadFromLS } from "../../services/helpers"
+import { getProductById } from "../../services/food-api"
+//////////
 const clouseBottun = document.querySelector(".clouse_modal")
 const modalBackdrop = document.querySelector('.modal-backdrop-product')
+const productMainList2 = document.querySelector('.product-list');
+const modal_window = document.querySelector('.modal-product')
 //////
-openBottun.addEventListener('click', toglModul)
+
 clouseBottun.addEventListener('click', toglModul)
 function toglModul(){
     modalBackdrop.classList.toggle('is-hidden')
 }
-/////
+/////////
+
+// const testButton = document.querySelector('.btn-basket')
+
+// testButton.addEventListener('click', testFunctD)
+
+// function testFunctD(event){
+//     console.log("dd")
+// }
+
+// const TestMod = localStorage.getItem(basket)
+// const LOCALSTORAGE_KEY = 'basket'
+// const basketTest = loadFromLS(LOCALSTORAGE_KEY) ?? [];
+// console.log(basketTest)
+// console.log(basketTest[0]._id)
+// // const CurrntTestProduct = loadFromLS(currentProduct)
+// const testCurren = "640c2dd963a319ea671e3861"
+// const currentProduct = await getProductById(testCurren);
+// console.log(currentProduct)
+
+
+
+
+
+
+async function handleClickOnLi(event) {
+
+    const closestRespItem = event.target.closest('.resp-item');
+    if (!closestRespItem) {
+        return;
+    }
+
+    const currentId2 = closestRespItem.dataset.id;
+    // console.log('Значение data-id:', currentId2);
+    
+    const testCurren = currentId2
+const currentProduct = await getProductById(testCurren);
+// console.log(currentProduct)
+
+
+    
+const marcap = `<div class="img" >
+<img class="photo" src="${currentProduct.img}" alt="${currentProduct.name}" loading="lazy"/>
+</div>
+<h2 class="name-product">${currentProduct.name}</h2>
+<div class="descr-product">
+<p class="category-product"><span class="style-word">Category:</span>${currentProduct.category}</p>
+<p class="size-product"><span class="style-word">Size:</span>${currentProduct.size}</p>
+<p class="popular-product"><span class="style-word">Popularity:</span>${currentProduct.popularity}</p>
+</div>
+<div class="footer-product_card">
+<p class="price-product">$${currentProduct.price}</p>
+<button type="button" class="btn-basket">Add</button>`
+modal_window.insertAdjacentHTML('beforeend', marcap);
+toglModul()
+
+}
+productMainList2.addEventListener("click", handleClickOnLi);
+
