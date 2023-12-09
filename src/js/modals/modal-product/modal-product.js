987de8e5e381d3c54024553eb5ca5eb4c501c8fa
Vladyslav6 <137646785+Vladyslav6@users.noreply.github.com>
2023-12-09 13:48:58 +0200

    fixModal2

--- src/js/modals/modal-product/modal-product.js
+++ src/js/modals/modal-product/modal-product.js
@@ -5,6 +5,7 @@ const clouseBottun = document.querySelector(".clouse_modal")
 const modalBackdrop = document.querySelector('.modal-backdrop-product')
 const productMainList2 = document.querySelector('.product-list');
 const modal_window = document.querySelector('.modal-product')
+const window_inModal = document.querySelector('.modal-product-inWindow')
 //////
 
 clouseBottun.addEventListener('click', toglModul)
@@ -47,25 +48,24 @@ async function handleClickOnLi(event) {
     // console.log('Значение data-id:', currentId2);
     
     const testCurren = currentId2
-const currentProduct = await getProductById(testCurren);
-// console.log(currentProduct)
-
-
-    
-const marcap = `<div class="img" >
-<img class="photo" src="${currentProduct.img}" alt="${currentProduct.name}" loading="lazy"/>
-</div>
-<h2 class="name-product">${currentProduct.name}</h2>
-<div class="descr-product">
-<p class="category-product"><span class="style-word">Category:</span>${currentProduct.category}</p>
-<p class="size-product"><span class="style-word">Size:</span>${currentProduct.size}</p>
-<p class="popular-product"><span class="style-word">Popularity:</span>${currentProduct.popularity}</p>
-</div>
-<div class="footer-product_card">
-<p class="price-product">$${currentProduct.price}</p>
-<button type="button" class="btn-basket">Add</button>`
-modal_window.insertAdjacentHTML('beforeend', marcap);
-toglModul()
+    const currentProduct = await getProductById(testCurren);
+    // console.log(currentProduct)
+ 
+    const marcap = `<div class="img" >
+    <img class="photo" src="${currentProduct.img}" alt="${currentProduct.name}" loading="lazy"/>
+    </div>
+    <h2 class="name-product">${currentProduct.name}</h2>
+    <div class="descr-product">
+    <p class="category-product"><span class="style-word">Category:</span>${currentProduct.category}</p>
+    <p class="size-product"><span class="style-word">Size:</span>${currentProduct.size}</p>
+    <p class="popular-product"><span class="style-word">Popularity:</span>${currentProduct.popularity}</p>
+    </div>
+    <div class="footer-product_card">
+    <p class="price-product">$${currentProduct.price}</p>
+    <button type="button" class="modal-button">Add</button>`
+    // modal_window.insertAdjacentHTML('beforeend', marcap);
+    window_inModal.innerHTML = marcap;
+    toglModul()
 
 }
 productMainList2.addEventListener("click", handleClickOnLi);
