dc5ddd0dd8722cb2ba7ecfe8700105e8b4435b52
nahirniy <115656838+nahirniy@users.noreply.github.com>
2023-12-09 13:44:40 +0000

    Deploying to gh-pages from @ nahirniy/Code-Storm@93b5601486907f70b2d1050838c88dcaa4659eb7 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import"./assets/styles-804e9a72.js";import{n as v,a as y}from"./assets/vendor-424513b8.js";function l(e,a){localStorage.setItem(e,JSON.stringify(a))}function n(e){const a=localStorage.getItem(e);try{return JSON.parse(a)}catch{return v.Notify.failure("Oops! Something went wrong..."),a}}function P(e){const{keyword:a,category:t,page:s,limit:r,...c}=e,u=Object.keys(c).length,i=new URLSearchParams({page:s,limit:r});if(a&&i.set("keyword",a),t&&i.set("category",t),u){const[b,h]=Object.entries(c)[0];i.set(b,h)}return i}function k(e,a,t){const s=t.findIndex(r=>r._id===a._id);s!==-1?t.splice(s,1):t.push(a),l(e,t)}const g="basket",w=n(g)??[],L=document.querySelector(".product-list");async function $(e){if(!e.target.closest(".btn-basket"))return;const t=e.target.closest(".resp-item").dataset.id,r=n("main products").filter(({_id:c})=>t===c)[0];k(g,r,w)}L.addEventListener("click",$);y.defaults.baseURL="https://food-boutique.b.goit.study/api";const S=async()=>{const{data:e}=await y.get("/products/categories");return e},d=async e=>{const a=P(e),{data:t}=await y.get("/products/",{params:a});return t},C=async e=>{const{data:a}=await y.get(`/products/popular?limit=${e}`);return a},f="/Code-Storm/assets/sprite-f073391d.svg",A={popularList:document.querySelector(".popular-list")};function E(e){const a=e.map(({name:t,img:s,category:r,size:c,popularity:u,_id:i})=>`<li class="popular-item" data-id="${i}">
+import"./assets/styles-e127b48f.js";import{n as v,a as d}from"./assets/vendor-424513b8.js";function u(e,s){localStorage.setItem(e,JSON.stringify(s))}function n(e){const s=localStorage.getItem(e);try{return JSON.parse(s)}catch{return v.Notify.failure("Oops! Something went wrong..."),s}}function k(e){const{keyword:s,category:t,page:a,limit:o,...r}=e,l=Object.keys(r).length,i=new URLSearchParams({page:a,limit:o});if(s&&i.set("keyword",s),t&&i.set("category",t),l){const[b,h]=Object.entries(r)[0];i.set(b,h)}return i}function P(e,s,t){const a=t.findIndex(o=>o._id===s._id);a!==-1?t.splice(a,1):t.push(s),u(e,t)}const f="basket",$=n(f)??[],w=document.querySelector(".product-list");async function L(e){if(!e.target.closest(".btn-basket"))return;const t=e.target.closest(".resp-item").dataset.id,o=n("main products").filter(({_id:r})=>t===r)[0];P(f,o,$)}w.addEventListener("click",L);d.defaults.baseURL="https://food-boutique.b.goit.study/api";const S=async()=>{const{data:e}=await d.get("/products/categories");return e},p=async e=>{const s=k(e),{data:t}=await d.get("/products/",{params:s});return t},C=async e=>{const{data:s}=await d.get(`/products/popular?limit=${e}`);return s},A=async()=>{const{data:e}=await d.get("/products/discount");return e},y="/Code-Storm/assets/sprite-f073391d.svg",E={popularList:document.querySelector(".popular-list")};function O(e){const s=e.map(({name:t,img:a,category:o,size:r,popularity:l,_id:i})=>`<li class="popular-item" data-id="${i}">
                     <div class="wrapper-img">
                         <img
-                            src="${s}"
+                            src="${a}"
                             alt="${t}"
                         />
                     </div>
@@ -9,32 +9,51 @@ import"./assets/styles-804e9a72.js";import{n as v,a as y}from"./assets/vendor-42
                     <div class="popular-info-top">
                         <h3 class="product-name">${t}</h3>
                         <button type="button" class="btn-ligth-basket js-btn-basket">
-                        <svg class="ligth-basket"><use width="12" height="12 "href="${f}#icon-basket"></use></svg>
+                        <svg class="ligth-basket"><use width="12" height="12 "href="${y}#icon-basket"></use></svg>
                         </button>
                         </div>
-                        <p class="product-category">Category: <span>${r.replace("_"," ")}</span></p>
+                        <p class="product-category">Category: <span>${o.replace("_"," ")}</span></p>
                         <div class="product-text">
-                            <p>Size: <span>${c}</span></p>
-                            <p>Popularity: <span>${u}</span></p>
+                            <p>Size: <span>${r}</span></p>
+                            <p>Popularity: <span>${l}</span></p>
                         </div>
                     </div>
-                </li>`);A.popularList.insertAdjacentHTML("beforeend",a.join(""))}const O=5;C(O).then(e=>E(e));const _=document.querySelector(".product-list"),B="main products";function p({results:e}){l(B,e);const a=e.map(t=>{let s=j(t.category),r=x(t.price);return`<li class="resp-item" data-id=${t._id} data-info="${t}">
+                </li>`);E.popularList.insertAdjacentHTML("beforeend",s.join(""))}const _=5;C(_).then(e=>O(e));const j={discountList:document.querySelector(".discount-list")};function B(e){const s=e.map(({name:t,img:a,_id:o,price:r})=>`<li class="discount-item" data-id="${o}">
+        <svg class="svg-discount" width="60" height="60">
+          <use href="${y}#icon-discount-mark"></use>
+        </svg>
+        <div class="wrap-img">
+          <img
+            src="${a}"
+            alt="${t}"
+          />
+        </div>
+        <div class="discount-product-info">
+          <h3 class="product-name">${t}</h3>
+          <p class="product-price">$${r}</p>
+          <button type="button" class="btn-basket">
+          <svg class="svg-basket">
+            <use class="href-icon" href="${y}#icon-basket"></use>
+          </svg>
+          </button>
+        </div>
+      </li>`);j.discountList.insertAdjacentHTML("beforeend",s.join(""))}A().then(e=>B(e));const M=document.querySelector(".product-list"),q="main products";function g({results:e}){u(q,e);const s=e.map(t=>{let a=x(t.category),o=T(t.price);return`<li class="resp-item" data-id=${t._id} data-info="${t}">
         <div class="img" >
           <img class="photo" src="${t.img}" alt="${t.name}" loading="lazy"/>
         </div>
         <h2 class="name-product">${t.name}</h2>
         <div class="descr-product">
-          <p class="category-product"><span class="style-word">Category:</span>${s}</p>
+          <p class="category-product"><span class="style-word">Category:</span>${a}</p>
           <p class="size-product"><span class="style-word">Size:</span>${t.size}</p>
           <p class="popular-product"><span class="style-word">Popularity:</span>${t.popularity}</p>
         </div>
         <div class="footer-product_card">
-          <p class="price-product">$${r}</p>
+          <p class="price-product">$${o}</p>
           <button type="button" class="btn-basket">
           <svg class="svg-basket" width="34" height="34">
-            <use class="href-icon" href="${f}#icon-basket"></use>
+            <use class="href-icon" href="${y}#icon-basket"></use>
           </svg>
           </button>
         </div>
-      </li>`}).join("");_.innerHTML=a}function j(e){return e.replace(/_/g," ")}function x(e){return Number.isInteger(e)?`${e}.00`:e.toFixed(2)}const m=document.querySelector(".category-list"),M=document.querySelector(".form"),q=document.querySelector(".keyword"),T=document.querySelector(".filters-all-param-list"),o="params of search",I={keyword:null,category:null,page:1,limit:9,byABC:!0};n(o)??l(o,I);async function F(){const a=(await S()).map(t=>`<option value="${t}" class="category-type">${K(t)}</option>`).join("");m.insertAdjacentHTML("afterbegin",a)}async function N(e){const a=e.target.value,t=n(o);let s;a!=="show-all"?s={...t,category:a}:s={...t,category:null};const r=await d(s);r?p(r):console.log("Ploha"),l(o,s)}async function z(e){const t={...n(o),keyword:e.target.value},s=await d(t);p(s),l(o,t)}async function R(e){e.preventDefault();const a=n(o),t=await d(a);t?p(t):console.log("Ploha")}function K(e){return e.replaceAll("_"," ")}async function D(e){let a,t;switch(e.target.value){case"byAtoZ":a="byABC",t=!0;break;case"byZtoA":a="byABC",t=!1;break;case"byCheaperfirst":a="byPrice",t=!0;break;case"byExpensivefirst":a="byPrice",t=!1;break;case"byPopular":a="byPopularity",t=!1;break;case"byNotpopular":a="byPopularity",t=!0;break;default:a="byABC",t=!0;break}G(a,t)}async function G(e,a){const t=n(o),{[Object.keys(t).pop()]:s,...r}=t,c={...r,[e]:a},u=await d(c);p(u),l(o,c)}async function H(){const e=n(o),a=await d(e);p(a)}M.addEventListener("submit",R);T.addEventListener("change",D);m.addEventListener("change",N);q.addEventListener("input",z);document.addEventListener("DOMContentLoaded",F);document.addEventListener("DOMContentLoaded",H);
+      </li>`}).join("");M.innerHTML=s}function x(e){return e.replace(/_/g," ")}function T(e){return Number.isInteger(e)?`${e}.00`:e.toFixed(2)}const m=document.querySelector(".category-list"),I=document.querySelector(".form"),F=document.querySelector(".keyword"),N=document.querySelector(".filters-all-param-list"),c="params of search",z={keyword:null,category:null,page:1,limit:9,byABC:!0};n(c)??u(c,z);async function D(){const s=(await S()).map(t=>`<option value="${t}" class="category-type">${G(t)}</option>`).join("");m.insertAdjacentHTML("afterbegin",s)}async function R(e){const s=e.target.value,t=n(c);let a;s!=="show-all"?a={...t,category:s}:a={...t,category:null};const o=await p(a);o?g(o):console.log("Ploha"),u(c,a)}async function H(e){const t={...n(c),keyword:e.target.value},a=await p(t);g(a),u(c,t)}async function K(e){e.preventDefault();const s=n(c),t=await p(s);t?g(t):console.log("Ploha")}function G(e){return e.replaceAll("_"," ")}async function U(e){let s,t;switch(e.target.value){case"byAtoZ":s="byABC",t=!0;break;case"byZtoA":s="byABC",t=!1;break;case"byCheaperfirst":s="byPrice",t=!0;break;case"byExpensivefirst":s="byPrice",t=!1;break;case"byPopular":s="byPopularity",t=!1;break;case"byNotpopular":s="byPopularity",t=!0;break;default:s="byABC",t=!0;break}Y(s,t)}async function Y(e,s){const t=n(c),{[Object.keys(t).pop()]:a,...o}=t,r={...o,[e]:s},l=await p(r);g(l),u(c,r)}async function J(){const e=n(c),s=await p(e);g(s)}I.addEventListener("submit",K);N.addEventListener("change",U);m.addEventListener("change",R);F.addEventListener("input",H);document.addEventListener("DOMContentLoaded",D);document.addEventListener("DOMContentLoaded",J);
 //# sourceMappingURL=commonHelpers2.js.map
