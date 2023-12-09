4bfa033026970479212a331fd3813323adbb9302
Vladyslav6 <vlad51284@gmail.com>
2023-12-09 21:38:48 +0200

    update2

--- src/js/modals/modal-subscription/modal-subscription.js
+++
@@ -1,6 +0,0 @@
-const clouseBottun = document.querySelector(".clouse_subscription")
-const modalSubs = document.querySelector('.modal-backdrop-subscription')
-clouseBottun.addEventListener('click', clouse_subs)
-function clouse_subs(){
-    modalSubs.classList.toggle('is-hidden')
-}
