90720401d850b8eaef65ca7bdeb2c05d29bb08ff
Vladyslav6 <vlad51284@gmail.com>
2023-12-11 01:38:20 +0200

    nexPush

--- src/js/modals/modal-product/modal-product.js
+++ src/js/modals/modal-product/modal-product.js
@@ -4,9 +4,11 @@ import sprite from '../../../img/icons/sprite.svg'
 //////////
 const clouseBottun = document.querySelector(".clouse_modal")
 const modalBackdrop = document.querySelector('.modal-backdrop-product')
-const productMainList2 = document.querySelector('.product-list');
 const modal_window = document.querySelector('.modal-product')
 const window_inModal = document.querySelector('.modal-product-inWindow')
+const productMainList2 = document.querySelector('.product-list');
+const popularProduct = document.querySelector('.popular-section')
+const DiscounProduct = document.querySelector('.discount-section')
 //////
 
 
@@ -44,7 +46,7 @@ function toglModul(){
 }
 function OnScroll(){
     document.body.style.overflow = '';
-    window.scrollTo(0, scrollPosition);
+    
 }
 /////////
 
@@ -60,21 +62,38 @@ function OnScroll(){
 
 
 async function handleClickOnLi(event) {
-
+let currentIdModal;
     const closestRespItem = event.target.closest('.resp-item');
-    if (!closestRespItem) {
-        return;
+    if (closestRespItem){
+        currentIdModal = closestRespItem.dataset.id;
+        console.log(currentIdModal)
     }
     const clickedButton = event.target.closest('.btn-basket');
     if (clickedButton) { 
         return; 
     }
+/////////
+const closestPopularItem = event.target.closest('.popular-item');
+if (closestPopularItem) {
+    currentIdModal = closestPopularItem.dataset.id;
+    console.log(currentIdModal)
+}
 
+const closestDiscounItem = event.target.closest('.discount-item');
+if(closestDiscounItem){
+    currentIdModal = closestDiscounItem.dataset.id;
+    console.log(currentIdModal)
+}
 
-    const currentIdModal = closestRespItem.dataset.id;
+if (!closestRespItem && !closestPopularItem && !closestDiscounItem) {
+    return;
+}
+
+/////////
+    // const currentIdModal = closestRespItem.dataset.id;
     const currentProduct = await getProductById(currentIdModal);
     
-    const marcap = `<div class="img_modal" >
+    const marcap = `<div class="img" >
     <img class="photo" src="${currentProduct.img}" alt="${currentProduct.name}" loading="lazy"/>
     </div>
     <h2 class="name-product">${currentProduct.name}</h2>
@@ -100,4 +119,5 @@ async function handleClickOnLi(event) {
 
 }
 productMainList2.addEventListener("click", handleClickOnLi);
-
+popularProduct.addEventListener("click", handleClickOnLi);
+DiscounProduct.addEventListener("click", handleClickOnLi);
