02b9173b0ac243b57848a0cc298d829b1f1eed56
ira089 <gvir2015@gmail.com>
2023-12-10 19:41:17 +0200

    Cart-content-ficha_1_com1

--- src/js/cart-content/product-list/pr-list-localStorage.js
+++ src/js/cart-content/product-list/pr-list-localStorage.js
@@ -1,15 +1,3 @@
-// забрираю массив с локалсторож
-export const funLoadFromLS = function loadFromLS(key) {
-  const dataCart = localStorage.getItem(key);
-  try {
-    // console.log(JSON.parse(dataCart));
-    return JSON.parse(dataCart);
-  } catch {
-    // console.log(dataCart);
-    return dataCart;
-  }
-};
-
 // удаление всех элементов из локалстораж при клике на del all
 export const funLoadLellAllLS = function LoadLellAllLS(key) {
   localStorage.removeItem(key);
