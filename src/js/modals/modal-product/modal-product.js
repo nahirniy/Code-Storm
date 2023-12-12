4bcb859d0aca5ba0fab3a5cbc229a008e292cc81
nahirniy <nahirniyy@gmail.com>
2023-12-12 10:33:28 +0200

    added functionallity for modal btn basket

--- src/js/modals/modal-product/modal-product.js
+++ src/js/modals/modal-product/modal-product.js
@@ -1,4 +1,8 @@
-import { loadFromLS } from '../../services/helpers';
+import {
+  loadFromLS,
+  updateAllIcon,
+  updateBasket,
+} from '../../services/helpers';
 import { getProductById } from '../../services/food-api';
 import { editText } from '../../services/helpers';
 import sprite from '../../../img/icons/sprite.svg';
@@ -12,15 +16,29 @@ const popularProduct = document.querySelector('.popular-section');
 const DiscounProduct = document.querySelector('.discount-section');
 //////
 
+const LOCALSTORAGE_KEY = 'basket';
+let basket;
+let currentIdModal;
+
+const handleClickBtn = () => optionsModal(currentIdModal, basket);
+
+function closeModal() {
+  document
+    .querySelector('.modal-button')
+    .removeEventListener('click', handleClickBtn);
+
+  toglModul();
+  OnScroll();
+}
+
 clouseBottun.addEventListener('click', function () {
   toglModul();
   OnScroll();
 });
 
-modalBackdrop.addEventListener('click', function (event) {
+modalBackdrop.addEventListener('click', event => {
   if (event.target == modalBackdrop) {
-    toglModul();
-    OnScroll();
+    closeModal();
   }
 });
 
@@ -38,20 +56,10 @@ function toglModul() {
 function OnScroll() {
   document.body.style.overflow = '';
 }
-/////////
-
-// const TestMod = localStorage.getItem(basket)
-// const LOCALSTORAGE_KEY = 'basket'
-// const basketTest = loadFromLS(LOCALSTORAGE_KEY) ?? [];
-// console.log(basketTest)
-// console.log(basketTest[0]._id)
-// // const CurrntTestProduct = loadFromLS(currentProduct)
-// const testCurren = "640c2dd963a319ea671e3861"
-// const currentProduct = await getProductById(testCurren);
-// console.log(currentProduct)
 
 async function handleClickOnLi(event) {
-  let currentIdModal;
+  basket = loadFromLS(LOCALSTORAGE_KEY) ?? [];
+
   const closestRespItem = event.target.closest('.resp-item');
   if (closestRespItem) {
     currentIdModal = closestRespItem.dataset.id;
@@ -79,25 +87,36 @@ async function handleClickOnLi(event) {
     return;
   }
 
+  const inStorage = basket.find(product => product._id == currentIdModal);
   const currentProduct = await getProductById(currentIdModal);
   const editTest = editText(currentProduct.category);
   const marcap = `<div class ="TestDiv1"><div class="img_modal2" >
-    <img class="photo-modal" src="${currentProduct.img}" alt="${currentProduct.name}" width="160" height="160"  loading="lazy"/>
+    <img class="photo-modal" src="${currentProduct.img}" alt="${
+    currentProduct.name
+  }" width="160" height="160"  loading="lazy"/>
     </div>
     <div class="testDiv">
     <h3 class="name-product2">${currentProduct.name}</h3>
     <div class="descr-product2">
     <p class="category-product"><span class="style-word">Category:</span>${editTest}</p>
-    <p class="size-product"><span class="style-word">Size:</span>${currentProduct.size}</p>
-    <p class="popular-product"><span class="style-word">Popularity:</span>${currentProduct.popularity}</p>
+    <p class="size-product"><span class="style-word">Size:</span>${
+      currentProduct.size
+    }</p>
+    <p class="popular-product"><span class="style-word">Popularity:</span>${
+      currentProduct.popularity
+    }</p>
     </div>
-    <div class="desc-text"> <p class="textModalp">${currentProduct.desc}</p></div>
+    <div class="desc-text"> <p class="textModalp">${
+      currentProduct.desc
+    }</p></div>
     </div>
     </div>
     <div class="footer-product_card2">
     <p class="price-product">$${currentProduct.price}</p></div>
     <div class = "end-modal">
-    <button type="button" class="modal-button"><span class ="textInButton">Add to</span><svg class="svg-basket2" width="18" height="18">
+    <button type="button" class="modal-button"><span class ="textInButton">${
+      inStorage ? 'Remove from' : 'Add to'
+    }</span><svg class="svg-basket2" width="18" height="18">
     <use class="href-icon" href="${sprite}#icon-basket"></use>
   </svg></button></div>`;
   window_inModal.innerHTML = marcap;
@@ -106,7 +125,42 @@ async function handleClickOnLi(event) {
   document.body.style.overflow = 'hidden';
 
   toglModul();
+
+  const currentBtn = document.querySelector('.modal-button');
+  currentBtn.addEventListener('click', handleClickBtn);
 }
+
+function findProduct(id) {
+  const mainProducts = loadFromLS('main products');
+  const popularProducts = loadFromLS('popular products');
+  const discountProducts = loadFromLS('discount products');
+
+  const allProducts = [
+    ...mainProducts,
+    ...popularProducts,
+    ...discountProducts,
+  ];
+
+  const currentProduct = allProducts.find(product => product._id === id);
+
+  return currentProduct;
+}
+
+function optionsModal(id, basket) {
+  const product = findProduct(id);
+  const currentButtons = document.querySelectorAll(`[data-button-id="${id}"]`);
+
+  updateBasket(LOCALSTORAGE_KEY, product, basket);
+  updateAllIcon([...currentButtons], id, basket);
+
+  const inBasket = basket.find(product => product._id === id);
+  const textInBtn = document.querySelector('.textInButton');
+
+  inBasket
+    ? (textInBtn.textContent = 'Remove from')
+    : (textInBtn.textContent = 'Add to');
+}
+
 productMainList2.addEventListener('click', handleClickOnLi);
 popularProduct.addEventListener('click', handleClickOnLi);
 DiscounProduct.addEventListener('click', handleClickOnLi);
