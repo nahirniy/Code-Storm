12bb5a53ab8afe89b4fd2b49ba57874802d4b3e4
ira089 <gvir2015@gmail.com>
2023-12-13 15:29:59 +0200

    Cart-counter_com1

--- src/js/cart-content/product-list/pr-list-markap.js
+++ src/js/cart-content/product-list/pr-list-markap.js
@@ -1,5 +1,48 @@
 import sprite from '../../../img/icons/sprite.svg';
-// функция разметки корзины с товаром
+// функция разметки корзины с товаром без кол-ва
+// export const funCartCreateMarkup = function createMarkup(arr) {
+//   return arr
+//     .map(
+//       ({
+//         name,
+//         category,
+//         size,
+//         price,
+//         img,
+//         _id,
+//       }) => `<li class="cart-item" data-id="${_id}">
+//       <div class="cart-item-wrap">
+//         <div class="cart-img-container">
+//           <img class="cart-img" src="${img}" alt="${name}" />
+//         </div>
+//         <div class="cart-img-text">
+//         <div class="cart-item-title-wrap"><h3 class="cart-item-title">${name}</h3>
+//         <button name="button" type="button" class="cart-btn-close" aria-label="Close modal window">
+//         <svg class="cart-icon-close">
+//           <use href="${sprite}#icon-close"></use>
+//         </svg>
+//       </button>
+//         </div>
+
+//           <div class="cart-info-container">
+//             <p class="cart-info">
+//               Category:
+//               <span>${category.replaceAll('_', ' ')}</span>
+//             </p>
+//             <p class="cart-info cart-info-overflow">
+//               Size:
+//               <span>${size}</span>
+//             </p>
+//           </div>
+//           <h3 class="cart-info-price">$${price.toFixed(2)}</h3>
+//         </div>
+//       </div>
+
+//     </li>`
+//     )
+//     .join('');
+// };
+// функция разметки корзины с товаром с кол-вом
 export const funCartCreateMarkup = function createMarkup(arr) {
   return arr
     .map(
@@ -11,34 +54,51 @@ export const funCartCreateMarkup = function createMarkup(arr) {
         img,
         _id,
       }) => `<li class="cart-item" data-id="${_id}">
-      <div class="cart-item-wrap">
-        <div class="cart-img-container">
-          <img class="cart-img" src="${img}" alt="${name}" />
-        </div>
-        <div class="cart-img-text">
-        <div class="cart-item-title-wrap"><h3 class="cart-item-title">${name}</h3>
-        <button name="button" type="button" class="cart-btn-close" aria-label="Close modal window">
-        <svg class="cart-icon-close">
-          <use href="${sprite}#icon-close"></use>
-        </svg>
-      </button>
-        </div>
-          
-          <div class="cart-info-container">
-            <p class="cart-info">
-              Category:
-              <span>${category.replaceAll('_', ' ')}</span>
-            </p>
-            <p class="cart-info cart-info-overflow">
-              Size:
-              <span>${size}</span>
-            </p>
+        <div class="cart-item-wrap">
+          <div class="cart-img-container">
+            <img class="cart-img" src="${img}" alt="${name}" />
           </div>
-          <h3 class="cart-info-price">$${price.toFixed(2)}</h3>
-        </div> 
-      </div>
-     
-    </li>`
+          <div class="cart-img-text">
+          <div class="cart-item-title-wrap"><span class="cart-item-title">${name}</span>
+          <button name="button" type="button" class="cart-btn-close">
+          <svg class="cart-icon-close">
+            <use href="${sprite}#icon-close"></use>
+            
+          </svg>
+        </button>
+          </div>
+
+            <div class="cart-info-container">
+              <p class="cart-info">
+                Category:
+                <span>${category.replaceAll('_', ' ')}</span>
+              </p>
+              <p class="cart-info cart-info-overflow">
+                Size:
+                <span>${size}</span>
+              </p>
+            </div>
+            <div class="counter-wrap">
+                <h3 class="cart-info-price">$${price}</h3>
+                <div class="cart-counter-wrap">
+                    <button class="cart-counter-btn-minus" >
+                    <svg class="cart-counter-icon">
+        <use href="/img/icons/sprite.svg#icon-minus"></use>
+      </svg>
+                    </button>
+                    <span class="cart-counter-text" id="item-${_id}-counter">1</span>
+                    <button class="cart-counter-btn-plus" >
+                    <svg class="cart-counter-icon" >
+        <use href="${sprite}#icon-plus"></use>
+      </svg>
+                    </button>
+                </div>
+                </div>
+                
+            </div>
+        </div>
+
+      </li>`
     )
     .join('');
 };
--- src/js/cart-content/product-list/product-list.js
+++ src/js/cart-content/product-list/product-list.js
@@ -2,6 +2,8 @@ import { funCartCreateMarkup } from './pr-list-markap';
 import { funLoadLellAllLS } from './pr-list-localStorage';
 import { loadFromLS } from '../../services/helpers';
 import { counterProducts } from '../../services/helpers';
+// import { onPlusFoo } from './cart-counter';
+// import { onMinusFoo } from './cart-counter';
 
 const refs = {
   cartContent: document.querySelector('.cart-content-wrap'),
@@ -18,12 +20,24 @@ const refs = {
 refs.cartItemContainer.addEventListener('click', onCartItem);
 refs.cartBtnDelAll.addEventListener('click', onCartDellAll);
 
+refs.cartItemContainer.addEventListener('click', onPlus);
+refs.cartItemContainer.addEventListener('click', onMinus);
+
 const LOCALSTORAGE_KEY = 'basket';
 let cartResults = loadFromLS(LOCALSTORAGE_KEY) ?? [];
+// добавляем в массив кол-во
+const quantity = 'quantity';
+cartResults.forEach(function (arr) {
+  arr[quantity] = 1;
+});
+console.log(cartResults);
 let arrayLength = cartResults.length;
 let newTotal = cartTotal(cartResults);
 
 cartProductList();
+
+// const plusBtn = document.querySelector('.cart-counter-btn-plus');
+//
 
 // колбек кнопки удалить все
 function onCartDellAll() {
@@ -42,7 +56,10 @@ function onCartDellAll() {
 // колбек удаления поштучно
 function onCartItem(evt) {
   const evtBtn = evt.target.closest('.cart-btn-close');
-  if (!evtBtn.nodeName) {
+
+  const classBtn = evtBtn.classList.value;
+  console.log(classBtn);
+  if (classBtn !== 'cart-btn-close') {
     return;
   }
 
@@ -107,9 +124,76 @@ function cartProductList() {
 }
 
 // функция для подсчета тотал
+// function cartTotal(results) {
+//   const totalPrice = results.reduce((total, result) => {
+//     return total + result.price;
+//   }, 0);
+//   return totalPrice;
+// }
+// заменяем функцию cartTotal с кол-вом
 function cartTotal(results) {
   const totalPrice = results.reduce((total, result) => {
-    return total + result.price;
+    return total + result.price * result.quantity;
   }, 0);
   return totalPrice;
+}
+//функции на плюс и минус
+function onPlus(evt) {
+  const evtBtn = evt.target.closest('.cart-counter-btn-plus');
+  const classBtn = evtBtn.classList.value;
+  console.log(classBtn);
+  if (classBtn !== 'cart-counter-btn-plus') {
+    return;
+  }
+  console.log(classBtn);
+  const evtWrap = evt.target.closest('.cart-counter-wrap');
+  // const itemId = console.dir(evtBtn);
+  const cartCounter = evtWrap.children[1];
+  // console.dir(cartCounter);
+  const numberText = parseInt(cartCounter.textContent);
+  cartCounter.textContent = numberText + 1;
+  console.log(cartCounter.textContent);
+  // создаём в объекте новую пару с количеством
+  const cartItemDell = evt.target.closest('.cart-item');
+  const cartIdDell = cartItemDell.dataset.id;
+  const indexItem = cartResults.findIndex(obj => obj._id === cartIdDell);
+  // console.log(indexItem);
+  cartResults[indexItem].quantity = parseInt(cartCounter.textContent);
+  // записали новый масив в локал сторож
+  // console.log(cartResults);
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(cartResults));
+  //пересчитали newTotal
+  let newTotal = cartTotal(cartResults);
+  // console.log(newTotal);
+  // записали в итог
+  refs.cartProductsSum.innerHTML = `$${String(newTotal.toFixed(2))}`;
+}
+
+function onMinus(evt) {
+  // console.log('нг');
+  const evtBtn = evt.target.closest('.cart-counter-btn-minus');
+  const classBtn = evtBtn.classList.value;
+  // console.log(classBtn);
+  if (classBtn !== 'cart-counter-btn-minus') {
+    return;
+  }
+  const evtWrap = evt.target.closest('.cart-counter-wrap');
+  const cartCounter = evtWrap.children[1];
+  const numberText = parseInt(cartCounter.textContent);
+  if (numberText === 1) {
+    return;
+  }
+  cartCounter.textContent = numberText - 1;
+  // создаём в объекте новую пару с количеством
+  const cartItemDell = evt.target.closest('.cart-item');
+  const cartIdDell = cartItemDell.dataset.id;
+  const indexItem = cartResults.findIndex(obj => obj._id === cartIdDell);
+  cartResults[indexItem].quantity = parseInt(cartCounter.textContent);
+  // записали новый масив в локал сторож
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(cartResults));
+  //пересчитали newTotal
+  let newTotal = cartTotal(cartResults);
+  // console.log(newTotal);
+  // записали в итог
+  refs.cartProductsSum.innerHTML = `$${String(newTotal.toFixed(2))}`;
 }
